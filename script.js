121ad3ae0cb166f0fd49f27f99a8a3899687648e
mss-souloftware <sufyan.souloftware@gmail.com>
2023-06-17 14:02:22 +0500

    bug fixing

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let tl = gsap.timeline({scrollTrigger:{
     trigger:"#main",
-    //markers:true, 
     start:"50% 50%",
     end:"100% 50%",
     scrub:2,
@@ -14,11 +13,12 @@ to("#top",{
     bottom:"-50%",
 }, 'a')
 .to("#top-h",{
-    bottom:"-50%",
+    top:"140%",
 }, 'a')
 .to("#bottom-h",{
-    top:"-60%",
+    top:"-40%",
 }, 'a')
 .to("#center h3",{
     marginTop:"0%",
+    scale:"1",
 }, 'a')
